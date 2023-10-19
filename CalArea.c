cc68945748e23a64e9d3a57557ee0e21c3522f76
urj2258 <liaqtumer296@gmail.com>
2023-10-19 23:18:56 +0500

    promting the user

---
+++ CalArea.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    double area,base,height;
+
+    printf("Enter the base of the triangle");
+    scanf("%lf",&base);
+
+    printf("Enter the height of the tringle");
+    scanf ("%lf",&height);
+}
