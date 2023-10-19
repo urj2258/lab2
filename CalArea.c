2c9897a29156e73ade3021a5839d426a86f59663
urj2258 <liaqtumer296@gmail.com>
2023-10-19 23:22:20 +0500

    completed code in qyestion 7

--- CalArea.c
+++ CalArea.c
@@ -3,9 +3,12 @@ int main()
 {
     double area,base,height;
 
-    printf("Enter the base of the triangle");
+    printf("Enter the base of the triangle: ");
     scanf("%lf",&base);
 
-    printf("Enter the height of the tringle");
+    printf("Enter the height of the tringle: ");
     scanf ("%lf",&height);
+
+    area = 0.5*base*height;
+    printf("The area is %f",area);
 }
