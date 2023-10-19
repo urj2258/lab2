c1738c2c53e6347f5244386d923437686e7063af
urj2258 <liaqtumer296@gmail.com>
2023-10-19 23:06:25 +0500

    Calculate fee

---
+++ dollar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>>
+int main()
+{
+    int dollar,fee;
+    printf("Enter your amount:" );
+    scanf("%lf",dollar);
+    fee = dollar/100*10;
+    fee = dollar -fee;
+    printf("your fee for the exchange is %.1lf",fee);
+
+    double pound 
+
+}
