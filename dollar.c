c9c5e8394e191b7bc26fd0402a3280c484d2623c
urj2258 <liaqtumer296@gmail.com>
2023-10-19 23:09:14 +0500

    completed code

--- dollar.c
+++ dollar.c
@@ -8,6 +8,9 @@ int main()
     fee = dollar -fee;
     printf("your fee for the exchange is %.1lf",fee);
 
-    double pound 
+    double pound = (0.5*dollar)*0.79;
+    double japanese = (0.5*dollar)*127.65;
+
+    printf("\nyou get: \n %.2lf GDP\n%.2lf JPY",pound, japanese);
 
 }
